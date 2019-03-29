003b703a94d298a0a2a9529f57c50288ac03cdc1
ye1209mx <1834924958@qq.com>
2019-03-29 17:55:00 +0800

    路由的测试,

---
+++ React/study/my-study/src/routes/Home/Home.js
@@ -0,0 +1,11 @@
+import React,{Component} from 'react';
+
+export default class Home extends Component{
+	render(){
+		return(
+			<div id="Home">
+				我是首页
+			</div>
+		)
+	}
+}
--- React/study/my-study/src/routes/Resource/Resource.js
+++ React/study/my-study/src/routes/Resource/Resource.js
@@ -1,2 +1,11 @@
 import React,{Component} from 'react';
 
+export default class Resource extends Component{
+	render(){
+		return (
+			<div id="Resource">
+				资源汇总
+			</div>
+		)
+	}
+}
